10ac18c97c698e0a0ea64cfda84eaa547467b619
Charlène Baldi <charlene.baldi6@gmail.com>
2024-05-23 20:42:57 +0200

    Premiers fichiers du projet

---
+++ Front-End/argentbank-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import Features from "./components/Features/Features"
+import Footer from "./components/Footer/Footer"
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Features />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
